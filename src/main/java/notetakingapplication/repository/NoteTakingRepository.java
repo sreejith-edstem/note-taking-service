52e25d261ab969609aab679f2f66fce56a1c2994
sreejith-edstem <sreejith.s@edstem.com>
2023-11-24 15:35:09 +0530

    removed-unwanted-from-repository

--- src/main/java/notetakingapplication/repository/NoteTakingRepository.java
+++ src/main/java/notetakingapplication/repository/NoteTakingRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface NoteTakingRepository extends JpaRepository<Note, Long> {
-    List<Note> findAllByIsDeletedTrueOrderByUpdatedAtDesc();
     List<Note> findAllByIsDeletedFalseOrderByUpdatedAtDesc();
 
     List<Note> findAllByOrderByUpdatedAtDesc();
